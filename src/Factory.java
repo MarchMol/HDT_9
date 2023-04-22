ffd82d01481cb97a1a68a92860cbc79daf17fc53
MarchMol <98711734+MarchMol@users.noreply.github.com>
2023-04-21 22:30:27 -0600

    Comentarios que hacian falta

--- src/Factory.java
+++ src/Factory.java
@@ -1,12 +1,14 @@
 public class Factory {
     public static <K extends Comparable<K>, V> Tree<K, V> TreeGen(String treeType) {
         if (treeType != null) {
+            //Se evalúa el tipo de mapeo escogido .toLowerCase
             switch (treeType.toLowerCase()) {
                 case "avl":
                     return new AVLTree<>();
                 case "rbt":
                     return new RedBlackTree<>();
                 default:
+                    //Si el ingreso es invalido se ejecuta una excepción
                     throw new IllegalArgumentException("Invalid tree type");
             }
         } else{
